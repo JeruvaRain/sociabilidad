3633b3d3266417418876aff27c2c9c644379e9c3
Jeruva Rain <jurivera@ug.uchile.cl>
2020-11-27 18:56:34 -0300

    Actualizado a los nuevos valores. Ahora debería de funcionar con tus obj.

--- preguntas_sociabilidad.R
+++ preguntas_sociabilidad.R
@@ -10,7 +10,7 @@ library(data.table)
 
 #Archivo
 PNUD_IDH_2012_SPSS <- read_sav("PNUD IDH 2012 SPSS.sav")
-xView(PNUD_IDH_2012_SPSS)
+View(PNUD_IDH_2012_SPSS)
 
 #################################
 #SEPARAR LAS PREGUNTAS DEL PNUD_IDH_2012_SPSS A UN DATA FRAME APARTE
@@ -23,59 +23,89 @@ xView(PNUD_IDH_2012_SPSS)
                         P17.11 = PNUD_IDH_2012_SPSS$P17.11, P17.12 = PNUD_IDH_2012_SPSS$P17.12
                         )
 #################################
+# Remover valores por encima del 10
 
-#
-# Crear una matrix con los mean
+#17.1
+p17_all$P17.1[p17_all$P17.1 == 77] <- NA
+p17_all$P17.1[p17_all$P17.1 == 88] <- NA
+p17_all$P17.1[p17_all$P17.1 == 99] <- NA
+#17.2
+p17_all$P17.2[p17_all$P17.2 == 77] <- NA
+p17_all$P17.2[p17_all$P17.2 == 88] <- NA
+p17_all$P17.2[p17_all$P17.2 == 99] <- NA
+#17.3 
+p17_all$P17.3[p17_all$P17.3 == 77] <- NA
+p17_all$P17.3[p17_all$P17.3 == 88] <- NA
+p17_all$P17.3[p17_all$P17.3 == 99] <- NA
 
-Matrix_pnud <- matrix(ncol = 12, byrow= TRUE)
-  colnames(Matrix_pnud) <- c("P17.1", "P17.2", "P17.3", "P17.4", "P17.5", "P17.6", "P17.7",
-                             "P17.8", "P17.9", "P17.10", "P17.11", "P17.12")
-Matrix_pnud <- as.table(Matrix_pnud)
+p17_all$P17.4[p17_all$P17.4 == 77] <- NA
+p17_all$P17.4[p17_all$P17.4 == 88] <- NA
+p17_all$P17.4[p17_all$P17.4 == 99] <- NA
 
-Matrix_pnud[,1] = p17_all_mean$P17.1
-Matrix_pnud[,2] = p17_all_mean$P17.2
-Matrix_pnud[,3] = p17_all_mean$P17.3
-Matrix_pnud[,4] = p17_all_mean$P17.4
-Matrix_pnud[,5] = p17_all_mean$P17.5
-Matrix_pnud[,6] = p17_all_mean$P17.6
-Matrix_pnud[,7] = p17_all_mean$P17.7
-Matrix_pnud[,8] = p17_all_mean$P17.8
-Matrix_pnud[,9] = p17_all_mean$P17.9
-Matrix_pnud[,10] = p17_all_mean$P17.10
-Matrix_pnud[,11] = p17_all_mean$P17.11
-Matrix_pnud[,12] = p17_all_mean$P17.12
+p17_all$P17.5[p17_all$P17.5 == 77] <- NA
+p17_all$P17.5[p17_all$P17.5 == 88] <- NA
+p17_all$P17.5[p17_all$P17.5 == 99] <- NA
 
-# Matrix convertida a data frame
+p17_all$P17.6[p17_all$P17.6 == 77] <- NA
+p17_all$P17.6[p17_all$P17.6 == 88] <- NA
+p17_all$P17.6[p17_all$P17.6 == 99] <- NA
 
-# Para graficar se utilizó dataf_pnud, quedó probado que funciona mejor que 
-# las otras data frames.
-dataf_pnud <- as.data.frame(dataf_pnud)
+p17_all$P17.7[p17_all$P17.7 == 77] <- NA
+p17_all$P17.7[p17_all$P17.7 == 88] <- NA
+p17_all$P17.7[p17_all$P17.7 == 99] <- NA
 
-# Para ordenar de menor a mayor la data.frame:
+p17_all$P17.8[p17_all$P17.8 == 77] <- NA
+p17_all$P17.8[p17_all$P17.8 == 88] <- NA
+p17_all$P17.8[p17_all$P17.8 == 99] <- NA
 
-dataf_pnud_ord <- dataf_pnud %>%
-  arrange(Freq)
+p17_all$P17.9[p17_all$P17.9 == 77] <- NA
+p17_all$P17.9[p17_all$P17.9 == 88] <- NA
+p17_all$P17.9[p17_all$P17.9 == 99] <- NA
+
+p17_all$P17.10[p17_all$P17.10 == 77] <- NA
+p17_all$P17.10[p17_all$P17.10 == 88] <- NA
+p17_all$P17.10[p17_all$P17.10 == 99] <- NA
+
+p17_all$P17.11[p17_all$P17.11 == 77] <- NA
+p17_all$P17.11[p17_all$P17.11 == 88] <- NA
+p17_all$P17.11[p17_all$P17.11 == 99] <- NA
+
+p17_all$P17.12[p17_all$P17.12 == 77] <- NA
+p17_all$P17.12[p17_all$P17.12 == 88] <- NA
+p17_all$P17.12[p17_all$P17.12 == 99] <- NA
+
+# Obtener el promedio
+
+p17_all_mean <- colMeans(p17_all, na.rm = TRUE)
+
+# Convertir a data frame
+p17_all_mean_df <- data.frame(p17_all_mean)
+setDT(p17_all_mean_df, keep.rownames = TRUE)
+names(p17_all_mean_df) <- c("Pregunta", "Valor")
 
 # PLOTTING THE MEAN
 
 # Points
 ggplot()+
-  geom_point(dataf_pnud, mapping = aes(Var2, Freq))+
+  geom_point(p17_all_mean_df, mapping = aes(Pregunta, Valor))+
   ggtitle("Promedios en preguntas de sociabilidad")+
   xlab("")+
   ylab("")
 
 # Bars
 ggplot()+
-  geom_bar(dataf_pnud, mapping = aes(Var2, Freq), stat = "identity", fill = c(2:13))+
+  geom_bar(p17_all_mean_df, mapping = aes(Pregunta, Valor), stat = "identity", fill = c(2:13))+
   ggtitle("Promedios: preguntas de sociabilidad")+
     xlab("")+
     ylab("")
 
 # Bars pero ordenado de Mayor a menor
 ggplot()+
-  geom_bar(dataf_pnud_ord, mapping = aes(reorder(Var2, -Freq), Freq), 
+  geom_bar(p17_all_mean_df, mapping = aes(reorder(Pregunta, -Valor), Valor), 
            stat = "identity", fill = c(2:13))+
   ggtitle("Promedios: preguntas de sociabilidad")+
   xlab("")+
   ylab("")
+
+#
+
